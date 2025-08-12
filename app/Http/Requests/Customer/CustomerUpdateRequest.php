a87151619b1867c6c6cb41e77f0b673621cff710
Chi Long <chilong03082003@gmail.com>
2025-08-12 09:26:44 +0700

    update

--- app/Http/Requests/Customer/CustomerUpdateRequest.php
+++ app/Http/Requests/Customer/CustomerUpdateRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests\Customer;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-abstract class CustomerUpdateRequest extends FormRequest
+class CustomerUpdateRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
